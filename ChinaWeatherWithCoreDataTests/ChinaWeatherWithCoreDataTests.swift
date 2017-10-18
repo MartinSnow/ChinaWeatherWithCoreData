766f3bc4d2b785690de0a6346f5fa2f86e7ea5c7
Your Name <you@example.com>
2017-10-18 08:06:45 +0800

    Initial Commit

---
+++ ChinaWeatherWithCoreDataTests/ChinaWeatherWithCoreDataTests.swift
@@ -0,0 +1,36 @@
+//
+//  ChinaWeatherWithCoreDataTests.swift
+//  ChinaWeatherWithCoreDataTests
+//
+//  Created by Ma Ding on 17/10/18.
+//  Copyright © 2017年 Ma Ding. All rights reserved.
+//
+
+import XCTest
+@testable import ChinaWeatherWithCoreData
+
+class ChinaWeatherWithCoreDataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
